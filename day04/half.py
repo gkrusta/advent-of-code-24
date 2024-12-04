d9599a6899862e18640161df7b8201dccf365993
gkrusta <krustagabriela@gmail.com>
2024-12-04 17:59:32 +0100

    on 4th day

---
+++ day04/half.py
@@ -0,0 +1,40 @@
+word = "XMAS"
+
+DIR = [
+    (0, 1), #right
+    (0, -1), #left
+    (1, 0), #down
+    (-1, 0), #up
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1)
+]
+
+
+def find_word(grid, row, col, x, y):
+    for i in range(len(word)):
+        posx, posy = row + i * x, col + i * y
+        if posx < 0 or posx >= len(grid) or posy < 0 or posy >= len(grid[0]) or grid[posx][posy] != word[i]:
+            return False
+    return True
+
+
+def main():
+    file = open("input.txt", "r")
+    grid = [line.strip() for line in file.readlines()]
+    print("GRID:", grid)
+    rows = len(grid)
+    cols = len(grid[0])
+    count = 0
+    print("rows:", rows)
+    print("cols:", cols)
+    for i in range(rows):
+        for j in range(cols):
+            for x, y in DIR:
+                if find_word(grid, i, j, x, y):
+                    count +=1
+    print("\ncount:", count)
+
+
+main()
